678f86a4f499d9e98668da2c43ad8b7ee45b935f
Théophile Tesseraud <theophiletesseraud@linux.home>
2020-10-23 14:43:09 +0200

    ajout des fonctions de bases (fragile)

--- src/metric.py
+++ src/metric.py
@@ -20,37 +20,63 @@ test=[("id_humain_1","102466751"),("id_humain_2","107985721"),("id_humain_3","79
 
 def get_info_gene(liste_tuple_gene,filegff):
 	dico_info_gene=dict()
-	for j in liste_tuple_gene:
+	for tuple_gene in liste_tuple_gene:
 		limit_info = dict( gff_type=["gene"])
 		examiner = GFFExaminer()
 		in_handle = open(filegff)
 		for rec in GFF.parse(in_handle, limit_info=limit_info):
 			for i in rec.features:
-				texte="GeneID:"+j[1]
+				texte="GeneID:"+tuple_gene[1]
 				if i.qualifiers['Dbxref'][0]==texte:
 					localisation=str(i.location)
 					localisation=localisation.split('](')
 					localisation[0]=localisation[0][1:]
 					localisation[1]=localisation[1][:-1]
 					localisation[0]=localisation[0].split(':')
-					info=info_gene(id_human=j[0],id_species=j[1],start=localisation[0][0],end=localisation[0][1],strand=localisation[1])
+					info=info_gene(id_human=tuple_gene[0],id_species=tuple_gene[1],start=localisation[0][0],end=localisation[0][1],strand=localisation[1])
 					dico_info_gene[info.id_species]=info
 	in_handle.close()
 	return dico_info_gene
 	
-get_info_gene(test,in_file)
 
-def cut_fasta_gene(dico_info_gene,fasta)
-	list_seq_fasta=list()
-	
-	return list_seq_fasta
+from Bio import SeqIO
+import gzip
+from Bio.Alphabet import generic_dna
 
+
+
+
+def cut_fasta_gene(dico_info_gene,fasta):
+	dico_seq_fasta=dict()
+	sequence_gene=list()
+	records = list(SeqIO.parse(fasta, "fasta"))
+	fasta_sequence=str(records[0].seq)
+	for i in dico_info_gene.values():
+		start_gene=int(i.start)
+		end_gene=int(i.end)
+		strand=i.strand
+		nom_gene=i.id_species
+		sequence_gene=fasta_sequence[start_gene:end_gene]
+		dico_seq_fasta[str(i.id_human)]=[sequence_gene,nom_gene,i.id_human]
+	return dico_seq_fasta
+
+fasta="test.fna"
 
 
 def calcul_GC(list_sequence_gene):
 	taux_GC=list()
-	for i in list_sequence:
-		GC=GC(i)
-		taux_GC.append(GC)
+	for i in list_sequence_gene:
+		test=GC(i)
+		taux_GC.append(test)
 	taux_GCGene=sum(taux_GC)/len(list_sequence_gene)
 	return taux_GCGene
+	
+
+dico=get_info_gene(test,in_file)
+sequence_genes_specie=cut_fasta_gene(dico,fasta)
+
+
+for i in sequence_genes_specie.values():
+	test=str(calcul_GC(i[0]))
+	print("le gène "+i[1] + " qui est orthologue du gène " + i[2] + " dans le génome de référence à un taux de GC de " + test +" %" )
+
