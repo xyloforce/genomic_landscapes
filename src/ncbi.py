12af19b46a470215c97a9a21f3e3662d1f3bf70b
xyloforce <fabsassolas@outlook.fr>
2020-10-29 19:18:36 +0100

    implanted get_genome

--- src/ncbi.py
+++ src/ncbi.py
@@ -1,13 +1,27 @@
 import sys
+import subprocess
 import json
+import zipfile
+
 
 def get_genome_from_ncbi(taxid):
     query_dict = summary("genome taxon ", taxid)
-    accession = query_dict[
-    command = "./datasets download genome --exclude-protein --exclude-rna " + taxid
+    print(query_dict)
+    accession = query_dict["assemblies"][0]["assembly"]["assembly_accession"]
+    command = "./datasets download genome --exclude-protein --exclude-rna accession " + accession
+    print("download started for assembly " + accession)
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, universal_newlines=True)
     output, error = process.communicate()
-    
+    downloaded_file = zipfile.ZipFile("ncbi_dataset.zip")
+    extracted_file = list()
+    file_list = downloaded_file.namelist()
+    for archive_file in file_list:
+        if archive_file[-3:] == "gff" or archive_file[-3:] == "fna":
+            downloaded_file.extract(archive_file, path="/tmp")
+            extracted_file.append(archive_file)
+    return extracted_file
+
+
 def summary(type, gene_ids): # split : create a summary method returning the true summary already parsed
     command = "./datasets summary " + type + gene_ids
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, universal_newlines=True)
