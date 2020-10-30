4545464c9fc954657f1cc5ddf04401eb09e59f22
Odd <odd.heurtel@gmail.com>
2020-10-30 16:42:30 +0100

    :up: function summary
    function summary ok with gene id of NCBI

--- src/ncbi.py
+++ src/ncbi.py
@@ -1,14 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+##########################################################
+# Library python for project M1 2020
+# version 0.1
+# datasets version use is 10.5
+##########################################################
 import sys
 import subprocess
 import json
 import zipfile
 
 
+PATH_DATASET = "./datasets"
+PATH_TO_DATA_DL = "/tmp/data"
+
+
+def summary(type, gene_id):
+    """
+    Return dic of summary for given gene_id NCBI.
+
+    command summary of NCBI is : « datasets summary [gene/genome] »
+    type is gene or genome
+    """
+    if type == "gene" or type == "genome":
+        command = "./datasets summary " + type + " gene-id " + str(gene_id)
+        #  execute command
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, universal_newlines=True)
+        output, error = process.communicate()
+        if error is None:
+            try:
+                query_dict = json.loads(output)
+                return query_dict
+            except Exception as e:
+                print(f"error catch {e}")
+                return None
+        else:
+            print(f"Error in CLI : {error}")
+            return None
+    else:
+        print(f"error in command type of 'datasets summary type', type must are « gene » or « genome » not {type}")
+
+
+
 def get_genome(taxid):
     """
     from taxid get genome file with gff and fna, extracts them to /tmp and return filelist
     """
-    query_dict = summary("genome taxon ", taxid)
+    query_dict = summary("genome taxon ", str(taxid))
     print(query_dict)
     accession = query_dict["assemblies"][0]["assembly"]["assembly_accession"]
     command = "./datasets download genome --exclude-protein --exclude-rna accession " + accession
@@ -25,15 +64,17 @@ def get_genome(taxid):
     return extracted_file
 
 
-def summary(type, gene_ids):
-    """
-    return dic of summary for given gene_ids
-    """
-    command = "./datasets summary " + type + gene_ids
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    output, error = process.communicate()
-    try:
-        query_dict = json.loads(output)
-    except:
-        print("error intercepted")
-    return query_dict
+
+
+#  test if datasets is installed
+try:
+    process = subprocess.Popen("./datasets version".split(), stdout=subprocess.PIPE, universal_newlines=True)
+    stdout, stderr = process.communicate()
+    print(f"datasets is present and this version is {stdout.strip()}")
+except FileNotFoundError as e:
+    print("You have to datasets install on your computer, you can try : « python3 -m pip install ncbi-datasets-pylib »\nOr you can download datasets programme from « https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets »")
+    sys.exit()
+
+if __name__ == '__main__':
+    print(summary("gene", 920835))
+    pass
