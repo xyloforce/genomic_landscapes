8a01e30d2ec1ca7904f29e70d113928f9d0906d5
Odd <odd.heurtel@gmail.com>
2020-10-30 19:06:52 +0100

    :up: function get_genome download is OK
    le téléchargement est bon, il se fait.
    Reste à décompresser et gérer cette étape
    Puis débugger la fonction pour vérifier les erreurs possibles

--- src/ncbi.py
+++ src/ncbi.py
@@ -10,21 +10,33 @@ import sys
 import subprocess
 import json
 import zipfile
+import time
 
 
 PATH_DATASET = "./datasets"
 PATH_TO_DATA_DL = "/tmp/data"
+SUBTYPE_LIST = ["accession", "taxon", "gene-id", "symbol"] #  from NCBI documentation
+VERBOSE = True # simple verbose mode, recommandation to false
 
 
-def summary(type, gene_id):
+def summary(type, value, subtype=None):
     """
-    Return dic of summary for given gene_id NCBI.
+    Return dic from dataset command summary of NCBI.
+    By default is summary of gene ID.
 
-    command summary of NCBI is : « datasets summary [gene/genome] »
+    command summary of NCBI is : « datasets summary [gene/genome] [subtype] [value] »
     type is gene or genome
+    subtype is optionnel and from SUBTYPE_LIST
     """
     if type == "gene" or type == "genome":
-        command = "./datasets summary " + type + " gene-id " + str(gene_id)
+        if subtype is None:
+            command = ' '.join((PATH_DATASET, "summary", type, "gene-id", str(value)))
+        elif subtype in SUBTYPE_LIST:
+            command = ' '.join((PATH_DATASET, "summary", type, subtype, str(value)))
+        else:
+            print(f"subtype {subtype} incorrect")
+            return None
+
         #  execute command
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, universal_newlines=True)
         output, error = process.communicate()
@@ -45,25 +57,42 @@ def summary(type, gene_id):
 
 def get_genome(taxid):
     """
-    from taxid get genome file with gff and fna, extracts them to /tmp and return filelist
+    From taxid get genome file with gff and fna, extracts them and return filelist.
+
+    taxid is a taxid NCBI exemple : human, 10116, "Mus Musculus"
     """
-    query_dict = summary("genome taxon ", str(taxid))
-    print(query_dict)
+    query_dict = summary("genome", str(taxid), subtype="taxon")
+    # get assembly_accession code of taxid from query_dict
+    # TODO: prendre le référence sinon faudra choisir…
     accession = query_dict["assemblies"][0]["assembly"]["assembly_accession"]
-    command = "./datasets download genome --exclude-protein --exclude-rna accession " + accession
+    command = ' '.join((PATH_DATASET, "download genome --exclude-protein --exclude-rna accession", accession))
     print("download started for assembly " + accession)
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, universal_newlines=True)
+
+    # execute command
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if VERBOSE:
+        # print download progressing
+        while True:
+            output = process.stdout.readline()
+            if process.poll() is not None:
+                break
+            if output:
+                print(output.decode('utf-8').rstrip())
     output, error = process.communicate()
-    downloaded_file = zipfile.ZipFile("ncbi_dataset.zip")
-    extracted_file = list()
-    file_list = downloaded_file.namelist()
-    for archive_file in file_list:
-        if archive_file[-3:] == "gff" or archive_file[-3:] == "fna":
-            downloaded_file.extract(archive_file, path="/tmp")
-            extracted_file.append(archive_file)
-    return extracted_file
-
-
+    if error is None:
+        # if ok, we unzip the genome file
+        sys.exit()
+        downloaded_file = zipfile.ZipFile("ncbi_dataset.zip")
+        extracted_file = list()
+        file_list = downloaded_file.namelist()
+        for archive_file in file_list:
+            if archive_file[-3:] == "gff" or archive_file[-3:] == "fna":
+                downloaded_file.extract(archive_file, path="/tmp")
+                extracted_file.append(archive_file)
+        return extracted_file
+    else:
+        print(f"error in command line : {error}")
+    sys.exit()
 
 
 #  test if datasets is installed
@@ -71,10 +100,11 @@ try:
     process = subprocess.Popen("./datasets version".split(), stdout=subprocess.PIPE, universal_newlines=True)
     stdout, stderr = process.communicate()
     print(f"datasets is present and this version is {stdout.strip()}")
-except FileNotFoundError as e:
+except FileNotFoundError:
     print("You have to datasets install on your computer, you can try : « python3 -m pip install ncbi-datasets-pylib »\nOr you can download datasets programme from « https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets »")
     sys.exit()
 
 if __name__ == '__main__':
-    print(summary("gene", 920835))
+    # print(summary("gene", 920835))
+    get_genome(2697049)
     pass
