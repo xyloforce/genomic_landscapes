d8180b4a5d5d047d6961c85dd0eef20fb6654c7e
xyloforce <fabsassolas@outlook.fr>
2020-11-24 23:33:41 +0100

    another typo

--- classSpecies.py
+++ classSpecies.py
@@ -27,7 +27,7 @@ class species:
             json.dump(self.genes, file_genes)
         info_species = self.taxid + "_infos_species.txt"
         with open(info_species, "w") as file_infos:
-            file_infos.write(self.species)
+            file_infos.write(self.species + "\n")
             file_infos.write("; ".join(self.lineage))
         compressed = path + self.taxid + ".zip"
         with zipfile.ZipFile(compressed, "w") as file_archive:
