907eb4e7c39a86427c5348bb2dbd047f1afe9c2d
Théophile Tesseraud <theophiletesseraud@linux.home>
2020-12-05 16:32:32 +0100

    prise en compte du nouveaux format (nom espèce au lieu du taxid)

--- src/metric.py
+++ src/metric.py
@@ -10,7 +10,7 @@ import os.path
 
 class info_gene(dict):
     """
-    Define an object who take information about one gene.
+    Defines an info_gene object which takes coordinate information of a gene and its exons as well as their respective nucleotide sequences..
     """
     def __init__(self, id_species):
         self.id_species = id_species
@@ -30,15 +30,6 @@ class info_gene(dict):
     def __repr__(self):
         return "le gene est le "+ self.id_species+ " qui va de " + str(self.genes[0]) + " à "+ str(self.genes[1]) + " sur le  chromosome " + str(self.chr)
 
-    def get_genes(self):
-        return self.genes
-       
-    def get_exons(self):
-        return self.exons
-
-    def get_chromosome(self):
-        return self.chr
-       
     def set_exons(self,exons):
         self.exons.update(exons)
       
@@ -65,6 +56,11 @@ class info_gene(dict):
 
 
 def get_intron_size(dict_gene):
+    """
+    IN: info_gene object dictionary
+    calculate the size of the introns from an info_gene object (if this one has the corodnnées of the exons) and which stores it in this same info_gene 
+    object
+    """
     for gene in dict_gene.values():
         # calculating introns size
         list_taille_exons = []
@@ -81,10 +77,9 @@ def get_intron_size(dict_gene):
 	
 def parsingGFF(dico_geneID, fileGFF):
     """
-    Get start and stop exon from GFF file.
-
-    IN: GFF and a gene ID list
-    OUT: dict[geneID] = [(exon start, exon stop)…]
+    get information about coordonnee of gene and exons
+    IN: un chemin vers un fichier gff et un dictionnaire d'objet info_gene
+    OUT: dict[geneID] = info_gene
     """
     geneID=list()
     for i in dico_geneID.keys():
@@ -133,40 +128,40 @@ def parsingGFF(dico_geneID, fileGFF):
 
 
 def parsing_fasta(dico_info_gene, fasta):
-    #parsage du fasta
+    #fasta parsing
     records = list(SeqIO.parse(fasta, "fasta"))
     
-    #parcours des objets info_gene
+    #browse of info_gene objects
     for info_gene in dico_info_gene.values():
         for fasta in records:
         
-            #ne prendre que la séquence fasta qui nous intéresse
+            #take only the fasta sequence of our info_gene object
             if fasta.id == info_gene.chr:
             
-                #transformer la sequence fasta en une chaine de caractère
+                #transform the fasta sequence into a character string
                 fasta_sequence = str(fasta.seq)
                 
-                #récupération séquence du gène
+                #gene sequence recovery
                 start_gene= int(info_gene.genes[0])
                 end_gene= int(info_gene.genes[1])
                 sequence_gene = fasta_sequence[start_gene:end_gene]
                 info_gene.set_sequence_gene(sequence_gene)
                 
-                #récupération séquence région flanquante avant le gène
+                #flanking region sequence recovery before the gene
                 limite_flanking_avant=start_gene-5000
                 if limite_flanking_avant<0:
                     limite_flanking_avant=0
                 sequence_flanking_avant=fasta_sequence[limite_flanking_avant:start_gene]
                 info_gene.set_sequence_fanquante_avant(sequence_flanking_avant)
                 
-                #récupération séquence région flanquante après le gène
+                #flanking region sequence recovery after the gene
                 limite_flanking_apres=end_gene + 5000
                 if limite_flanking_apres>len(fasta_sequence):
                     limite_flanking_apres=len(fasta_sequence)
                 sequence_flanking_apres=fasta_sequence[end_gene:limite_flanking_apres]
                 info_gene.set_sequence_fanquante_apres(sequence_flanking_apres)
                 
-                #récupération séquence des exons
+                #exon sequence recovery
                 for i in info_gene.exons.values():
                     start_exon=int(i[0])
                     end_exon=int(i[1])
@@ -175,24 +170,25 @@ def parsing_fasta(dico_info_gene, fasta):
                     info_gene.set_sequence_exon(sequence_exon)
     return dico_info_gene
 
-def calcul_GC(list_sequence_gene,type_GC):
+def calcul_GC(list_sequence,type_GC):
     """
     calcul GC rate from a list of sequence
     """
     taux_GC = list()
-    if len(list_sequence_gene)==0:
-        taux_GCGene='NA'
-        return taux_GCGene
-    for i in list_sequence_gene:
+    if len(list_sequence)==0:
+        taux_GC='NA'
+        return taux_GC
+    for i in list_sequence:
         if len(i)==0:
             continue
-        test = GC123(i)
-        test= test[int(type_GC)]
-        taux_GC.append(test)
-    taux_GCGene = sum(taux_GC)/len(list_sequence_gene)
-    return taux_GCGene
+        sequence = GC123(i)
+        sequence= sequence[int(type_GC)]
+        taux_GC.append(sequence)
+    taux_GC = sum(taux_GC)/len(list_sequence)
+    return taux_GC
 
 def taux_GC(objet):
+    "for an info_gene object. Calculate the different GC rates"
     list_seq_gene=[objet.seq_gene]
     objet.taux_GC_gene=calcul_GC(list_seq_gene,0)
     
@@ -208,6 +204,7 @@ def taux_GC(objet):
 
 
 def create_tab_metrics(set_human_gene,metrics):
+    "create a metric file"
     file_metric = open('metrics_{}.txt'.format(metrics),'w')
     file_metric.write("genome reference"+"\t")
     for humanID in set_human_gene:
@@ -218,7 +215,7 @@ def create_tab_metrics(set_human_gene,metrics):
 
 def write_tab_metrics(dico_metric,metrics,taxID):
     """
-    take a dictionnairy of metrics dico_metric[id_humans]=(metrics) and write the metrics to the last line of an output tabulate file
+    take a dictionnairy of metrics dico_metric[id_humans]=info_gene and write the metrics to the last line of an output tabulate file
     """
     read_metric=open('metrics_{}.txt'.format(metrics),'r')
     id_human=read_metric.readline()
@@ -245,20 +242,19 @@ def write_tab_metrics(dico_metric,metrics,taxID):
                     file_metric_2.write(str(valeur.taux_GC3_exon)+"\t")
                     aucune_ecriture=False
                     continue
-                if metrics=='taille_intron':
+                if metrics=='intron_size':
                      file_metric_2.write(str(valeur.taille_intron)+"\t")
                      aucune_ecriture=False
                      continue
-                if metrics=='taux_GC_flanquante_avant':
+                if metrics=='GC_flanking_region_before':
                     file_metric_2.write(str(valeur.taux_GC_flanquante_avant)+"\t")
                     aucune_ecriture=False
                     continue
-                if metrics=='taux_GC_flanquante_apres':
+                if metrics=='GC_flanking_region_after':
                     file_metric_2.write(str(valeur.taux_GC_flanquante_apres)+"\t")
                     aucune_ecriture=False
                     continue
         if aucune_ecriture==True:
             file_metric_2.write("NA"+"\t")
-            test=False                         
     file_metric_2.write("\n")
     file_metric_2.close()
